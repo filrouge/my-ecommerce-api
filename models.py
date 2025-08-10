c869518553e515eb3598cb2d09a1ca4034a53b04
filrouge <6169761+filrouge@users.noreply.github.com>
2025-08-10 15:39:30 +0200

    feat(register): add GET /api/auth/register route

---
+++ models.py
@@ -0,0 +1,29 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+
+class User:
+    _id_counter = 1
+
+    def __init__(self, email, nom, password, role="client"):
+        self.id = User._id_counter
+        User._id_counter += 1
+
+        self.email = email
+        self.nom = nom
+        self.password_hash = generate_password_hash(password)
+        self.role = role
+        self.date_creation = datetime.utcnow()
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "nom": self.nom,
+            "role": self.role,
+            "password_hash": self.password_hash,
+            "date_creation": self.date_creation.isoformat()
+        }
