b6399d9e3d67c83759a69263249ae8314af38092
filrouge <6169761+filrouge@users.noreply.github.com>
2025-09-07 13:44:57 +0200

    refactor(code-typing): Enrich main codes with explanations

--- model/database.py
+++ model/database.py
@@ -1,23 +1,27 @@
-'''
-    Point d'entrée pour la création de session SQLAlchemy
-'''
 from config import Config
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 DB_URL = Config.DATABASE_URL
 
-# Engine pour la database locale SQLite
-engine = create_engine(
+'''
+Point d'entrée pour la création de session SQLAlchemy:
+    - Engine pour database locale SQLite
+    - Creation de session individuelle
+    - Initialisation de la base
+'''
+
+engine: Engine = create_engine(
     DB_URL,
     connect_args={"check_same_thread": False},
     echo=False
     )
 
-# Creation de session individuelle
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
-# Initialisation de la base
 Base = declarative_base()
