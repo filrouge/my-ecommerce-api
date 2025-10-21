df19e29e762f0b95d86f28a2e861baa808ddeab9
filrouge <6169761+filrouge@users.noreply.github.com>
2025-10-21 15:11:16 +0200

    refactor(errors): Refactor & Update errors & schemas

--- app/errors_handlers.py
+++ app/errors_handlers.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
 from sqlalchemy.exc import SQLAlchemyError
-from app.core.exceptions.orm_errors import ORM_ERROR_MAP
+from app.core.exceptions.errors_maps import ORM_ERROR_MAP
 from app.core.exceptions.app_errors import ApplicationError
 
 from pydantic import ValidationError
-from app.schemas.errors.validation_schemas import (
-    ValidationErrorItem, ValidationErrorsSchema
+from app.schemas.errors.json_schemas import (
+    ValidationErrorItem, ValidationErrorSchema
 )
 
 
@@ -20,31 +20,29 @@ def register_error_handlers(app: Flask) -> None:
 
     @app.errorhandler(SQLAlchemyError)
     def handle_orm_exceptions(error: SQLAlchemyError):
-        # # Pour DEBUG seulement -- conf PROD/DEV centralisée ?
-        # if isinstance(error, IntegrityError):
-        #     return jsonify({
-        #         "error": f"Database - Contrainte d'intégrité violée \
-        #             ({error.orig})"
-        #         }), 409
+        exc_name = error.__class__.__name__  # ex: "IntegrityError"
+        if exc_name in ORM_ERROR_MAP:
+            code, info = next(iter(ORM_ERROR_MAP[exc_name].items()))
+            return jsonify({"error": info["error"]}), code
 
-        for exception_type, (code, msg) in ORM_ERROR_MAP.items():
-            if isinstance(error, exception_type):
-                # return jsonify({"error": f"Database - {msg}"}), code
-                return jsonify({"error": msg}), code
+        # for exception_type, (code, msg) in ORM_ERROR_MAP.items():
+        #     if isinstance(error, exception_type):
+        #         # return jsonify({"error": f"Database - {msg}"}), code
+        #         return jsonify({"error": msg}), code
 
         return jsonify({"error": "Database - Erreur interne inconnue"}), 500
     
     @app.errorhandler(ValidationError)
     def handle_validation_error(error: ValidationError):
-        """ Format en JSON les détails du ValidationError. """
+        """ Formate en JSON les détails du ValidationError. """
         errors = [
             ValidationErrorItem(
                 loc=err.get("loc", ()),
                 msg=err.get("msg", ""),
                 type=err.get("type", ""),
-                input=err.get("input"),
-                url=err.get("url")
+                # input=err.get("input"),
+                # url=err.get("url")
             ) for err in error.errors()
         ]
-        response = ValidationErrorsSchema(errors=errors)
+        response = ValidationErrorSchema(errors=errors)
         return jsonify(response.model_dump()), 422
