b68ad72f34c820dff64651e7a9176351acd8fc35
filrouge <6169761+filrouge@users.noreply.github.com>
2025-10-21 15:19:09 +0200

    update(database): Update app and config files

--- app/run.py
+++ app/run.py
@@ -1,7 +1,9 @@
+# import os
 from app import create_app
 
-app = create_app()
 
-
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    # debug = os.getenv("FLASK_ENV", "dev") == "dev"
+    app = create_app()
     app.run(debug=True)
+    # app.run(debug=debug)
--- app/spec.py
+++ app/spec.py
@@ -1,12 +1,25 @@
 from spectree import SpecTree
+from spectree.models import SecurityScheme
 
+
+# Initialisation de l'instance Spectree
 spec = SpecTree(
     "flask",
     title="My E-commerce API",
     version="1.0.0",
     description="Documentation API Flask (via Spectree)",
+
     servers=[{"url": "http://localhost:5000"}],
-    # Force pour que Swagger UI ne montre pas de "Servers"
-    # Sans "Servers" -> perte de la possibilité de cURL tests
+    # # Pour forcer Swagger UI à ne pas montrer "Servers" (-> plus de cURL tests)
     # servers=[],
+
+    # Sécurité appliquée globalement
+    security=[{"bearerAuth": []}],
+
+    # Schéma de sécurité pour Swagger UI
+    security_schemes=[
+        SecurityScheme(
+            name="auth_TOKEN",
+            data={"type": "apiKey", "name": "Authorization", "in": "header"},
+        )]
 )
